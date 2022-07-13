d703fa52afff7ee0bf81b6f6ec2c2e277de3faf8
Peter Kulisov <newfapman@gmail.com>
2022-07-13 10:57:42 +0100

    animation finished/anchor to the second page added

--- peterk/backend/views.py
+++ peterk/backend/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "backend/index.html")
+
+def portfolio(request):
+    return render(request, "backend/portfolio.html")
