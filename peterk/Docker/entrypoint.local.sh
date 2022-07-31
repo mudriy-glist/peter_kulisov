d8aaa3699f216c3e950ec0cb3792d053e7157e7d
mudriy-glist <newfapman@gmail.com>
2022-07-31 17:00:39 +0100

    changed docker // db added

--- peterk/Docker/entrypoint.local.sh
+++ peterk/Docker/entrypoint.local.sh
@@ -3,4 +3,8 @@
 # python manage.py migrate &&
 python manage.py collectstatic --no-input &&
 
+python manage.py makemigrations &&
+
+python manage.py migrate &&
+
 python manage.py runserver 0.0.0.0:8000
