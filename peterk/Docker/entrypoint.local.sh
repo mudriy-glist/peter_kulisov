0e3b6eaa04aa68d400ead9e77d6ea4267c775942
mudriy-glist <newfapman@gmail.com>
2022-07-31 17:19:08 +0100

    entrupoint changed/migrate added

--- peterk/Docker/entrypoint.local.sh
+++ peterk/Docker/entrypoint.local.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # python manage.py migrate &&
-python manage.py collectstatic --no-input &&
+python manage.py collectstatic --no-input &
 
-python manage.py makemigrations &&
+python manage.py makemigrations &
 
-python manage.py migrate &&
+python manage.py migrate &
 
 python manage.py runserver 0.0.0.0:8000
