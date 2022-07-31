0e900280360d9cdcca704fa99c9348bf38227373
mudriy-glist <newfapman@gmail.com>
2022-07-31 15:39:09 +0100

    formatted urls

--- peterk/backend/urls.py
+++ peterk/backend/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
+
 from . import views
 
-urlpatterns = [
-    path("", views.portfolio, name="portfolio"),
-    path("message", views.message, name="message")
-]
+urlpatterns = [path("", views.portfolio, name="portfolio"), path("message", views.message, name="message")]
