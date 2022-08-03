6e888927fa48921a068963289ca72e19681bdb1d
mudriy-glist <newfapman@gmail.com>
2022-08-03 23:16:34 +0100

    python -> python3

--- peterk/Docker/entrypoint.local.sh
+++ peterk/Docker/entrypoint.local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python manage.py makemigrations &
-python manage.py migrate &
-python manage.py collectstatic --no-input --clear &
+python3 manage.py makemigrations &
+python3 manage.py migrate &
+python3 manage.py collectstatic --no-input --clear &
 
 gunicorn peterk.wsgi -w 1 -b 0.0.0.0:8000 --timeout 120 --workers 5 --worker-class gevent
